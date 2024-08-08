498dc5eac54425359eba92741001b9eba60e1117
Aaron Lal <lucidlear@gmail.com>
2024-08-09 06:30:49 +1000

    Add project files.

---
+++ SwinAdventure/IdentifiableObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    public class IdentifiableObject
+    {
+        // Fields
+        private List<String> _identifiers;
+
+        // Constructor
+        public IdentifiableObject(string[] idents)
+        {
+            _identifiers = new List<string>();
+
+            if (idents.Length != idents.Distinct().Count()) // validation
+            {
+                Console.WriteLine("The array has duplicates, try again!");
+                return;
+            }
+
+            foreach (string id in idents) // add identifiers
+            {
+                _identifiers.Add(id.ToLower());
+            }
+        }
+
+        // Properties
+        public string FirstId
+        {
+            get { return _identifiers[0]; }
+        }
+
+        // Methods
+        public bool AreYou(string id)
+        {
+            string idLower = id.ToLower();
+
+            return _identifiers.Contains(idLower);
+
+        }
+
+        public void AddIdentifier(string id)
+        {
+            string idLower = id.ToLower();
+
+            
+            if (_identifiers.Contains(idLower)) // validation 
+            {
+                Console.WriteLine("There is already an identifier by this name!");
+                return;
+            }
+                               
+            _identifiers.Add(idLower);
+        }
+
+    }
+}
---
+++ SwinAdventure/TestSwinAdventure.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    [TestFixture]
+    public class TestSwinAdventure
+    {
+        // arrange
+        private IdentifiableObject _identifiableObject;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            string[] test = { "fred", "bob" };
+            _identifiableObject = new IdentifiableObject(test);
+        }
+
+        // act and assert
+        [Test]
+        public void TestAreYou() 
+        {
+            bool result = _identifiableObject.AreYou("fred");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void TestNotAreYou()
+        {
+            bool result1 = _identifiableObject.AreYou("wilma");
+            bool result2 = _identifiableObject.AreYou("boby");
+            Assert.That(result1 && result2, Is.False);
+        }
+
+        [Test]
+        public void TestCaseSensitive()
+        {
+            bool result1 = _identifiableObject.AreYou("FRED");
+            bool result2 = _identifiableObject.AreYou("bOB");
+
+            Assert.That(result1 && result2, Is.True);
+        }
+
+        [Test]
+        public void TestFirstID()
+        {
+            string result = _identifiableObject.FirstId;
+
+            Assert.That(result, Is.EqualTo("fred"));
+        }
+
+        [Test]
+        public void TestAddID()
+        {
+            _identifiableObject.AddIdentifier("wilma");
+
+            bool result1 = _identifiableObject.AreYou("wilma");
+            bool result2 = _identifiableObject.AreYou("fred");
+            bool result3 = _identifiableObject.AreYou("bob");
+
+            Assert.That(result1 && result2 && result3, Is.True);
+        }
+
+    }
+}
